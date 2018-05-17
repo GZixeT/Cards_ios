1ff5ffaf3d139ed1b40c21d48898ad7168f44be0
Георгий Зубков <allengzixet@gmail.com>
2018-05-17 12:59:03 +0300

    commit

--- Cards_ios/GameViewController.h
+++ Cards_ios/GameViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "Cards.h"
-//for test branch
+//for test branch to merge
 //for commit of tower
 @interface GameViewController : UIViewController{
     CGFloat _screenWidth;
